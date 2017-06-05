d21a1667d33e325e78f550cd725a2a29097bb869
EiSandi <eisandihtaywin95@gmail.com>
2017-06-05 23:14:32 +0800

    test

--- bot.py
+++ bot.py
@@ -12,11 +12,11 @@ requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
 ###VARIABLES THAT YOU NEED TO SET MANUALLY IF NOT ON HEROKU#####
 try:
         MESSAGE ="Hi, I'm Office Uncle and I am here to help you understand what insurance you have, your coverage and to answer FAQs.I respond to keywords (for example MRI, physiotherapy, reimbursement), however to get you started -here's some suggested keywords for you to try:\nGP; Specialist; Surgery; Pregnancy; Dentist"
-        TOKEN = "KMeMnjN43NFFWNYp3UfFbv7U"
+        TOKEN = "xoxb-192115241664-GwLVUSLTCWStYLg3X7aOtCmo"
         UNFURL = os.environ['UNFURL-LINKS']
 except:
         MESSAGE = "Hi, I'm Office Uncle and I am here to help you understand what insurance you have, your coverage and to answer FAQs.I respond to keywords (for example MRI, physiotherapy, reimbursement), however to get you started -here's some suggested keywords for you to try:\nGP; Specialist; Surgery; Pregnancy; ,Dentist"
-        TOKEN ="KMeMnjN43NFFWNYp3UfFbv7U"
+        TOKEN ="xoxb-192115241664-GwLVUSLTCWStYLg3X7aOtCmo"
         UNFURL = 'FALSE'
 ###############################################################
 
