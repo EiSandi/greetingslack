5e4c730e1d6f62737f4707b6a0594393c4c8a49c
EiSandi <eisandihtaywin95@gmail.com>
2017-06-06 15:45:34 +0800

    test for email

--- bot.py
+++ bot.py
@@ -26,7 +26,7 @@ def parse_join(message):
         x = requests.get("https://slack.com/api/im.open?token="+TOKEN+"&user="+m["user"]["id"])
         x = x.json()
 
-        result=requests.get("https://slack.com/api/users.profile.get?token="+TOKEN)
+        result=requests.get("https://slack.com/api/users.profile.get?token=xoxp-192695256963-193201029506-193298116261-f98cdd67eea31b3f2eb6d0aca8b7f6ae")
         result=result.json()
         # print result["profile"]["email"]
         print result
