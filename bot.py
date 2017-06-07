81b9caa612b4595612befcc0e729d84633400c1a
EiSandi <eisandihtaywin95@gmail.com>
2017-06-07 13:53:58 +0800

    test

--- bot.py
+++ bot.py
@@ -11,38 +11,20 @@ requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
 
 ###VARIABLES THAT YOU NEED TO SET MANUALLY IF NOT ON HEROKU#####
 try:
+        MESSAGE = "Hi, I'm Office Uncle and I am here to help you understand what insurance you have, your coverage and to answer FAQs.I respond to keywords (for example MRI, physiotherapy, reimbursement), however to get you started -here's some suggested keywords for you to try:\nGP; Specialist; Surgery; Pregnancy;Dentist"
         TOKEN = "xoxb-192115241664-GwLVUSLTCWStYLg3X7aOtCmo"
         UNFURL = os.environ['UNFURL-LINKS']
 except:
+        MESSAGE = "Hi, I'm Office Uncle and I am here to help you understand what insurance you have, your coverage and to answer FAQs.I respond to keywords (for example MRI, physiotherapy, reimbursement), however to get you started -here's some suggested keywords for you to try:\nGP; Specialist; Surgery; Pregnancy;Dentist
         TOKEN ="xoxb-192115241664-GwLVUSLTCWStYLg3X7aOtCmo"
         UNFURL = 'FALSE'
 ###############################################################
 
 def parse_join(message):
     m = json.loads(message)
-    print ("see m[user]",m)
     if (m['type'] == "team_join"):
         x = requests.get("https://slack.com/api/im.open?token="+TOKEN+"&user="+m["user"]["id"])
         x = x.json()
-
-
-        result=requests.get("https://slack.com/api/users.profile.get?token=xoxp-192695256963-193224358161-193226599361-991ffffc877a35af8d1c91270d855f42")
-        result=result.json()
-        text=result["profile"]["email"]
-        print("profile",result)
-        
-
-        data = text.split("@")
-        value=data[1]
-        print value
-
-        if (value=="gmail.com"):
-            MESSAGE = "Hi, I'm Office Uncle and I am here to help you understand what insurance you have, your coverage and to answer FAQs.I respond to keywords (for example MRI, physiotherapy, reimbursement), however to get you started -here's some suggested keywords for you to try:\nGP; Specialist; Surgery; Pregnancy;Dentist"
-
-        else:
-            MESSAGE="Hello!Welcome to Lumenlab"
-
-
         x = x["channel"]["id"]
         if (UNFURL.lower() == "false"):
           xx = requests.post("https://slack.com/api/chat.postMessage?token="+TOKEN+"&channel="+x+"&text="+urllib.quote(MESSAGE)+"&parse=full&as_user=true&unfurl_links=false")
